ea93237cfdcfe89788dbee1ceab7e98ac69d93b1
minosmlee <52672287+minosmlee@users.noreply.github.com>
2022-10-07 11:28:12 +0900

    Update main-loginkey.tf

--- infra/main-loginkey.tf
+++ infra/main-loginkey.tf
@@ -1,3 +1,17 @@
 resource "ncloud_login_key" "loginkey" {
   key_name = "key-workshop"
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "local_file" "login_key" {
+  filename        = format("./%s.pem", ncloud_login_key.loginkey.key_name)
+  content         = ncloud_login_key.loginkey.private_key
+  file_permission = "0400"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
